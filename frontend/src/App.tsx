6b57312cdc0e11331fc8f1060d6136d796b834c9
Mya Thanegi Soe <myasoe@berkeley.edu>
2025-07-31 11:15:43 -0400

    error resolved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import PortfolioTable from './PortfolioTable';
-=======
 import logo from './logo.svg';
 import './App.css';
 import { Dashboard } from './pages/Dashboard'
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <PortfolioTable />
-=======
       <Dashboard />
->>>>>>> Stashed changes
     </div>
   );
 }
